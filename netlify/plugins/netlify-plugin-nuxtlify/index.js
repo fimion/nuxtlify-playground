e59a36811e8c8a49a1ce4d5616dc5a3f4ae8a75a
fimion <fimion@gmail.com>
2021-02-28 11:44:19 -0500

    start with only the build plugin

--- netlify/plugins/netlify-plugin-nuxtlify/index.js
+++ netlify/plugins/netlify-plugin-nuxtlify/index.js
@@ -1,5 +1,13 @@
+/*
+[[redirects]]
+from="/*"
+to="/.netlify/functions/nuxtlify/:splat"
+status=200
+force=true
+ */
+
 module.exports = {
-  onBuild(context){
-    console.log(context);
-  }
+  onBuild(context) {
+    console.log(context)
+  },
 }
